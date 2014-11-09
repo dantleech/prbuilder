fb02a8166efd298ffb420df3eb96e6a927be3b72
dantleech <dan.t.leech@gmail.com>
2014-11-09 21:52:02 +0100

    Persist pull requests in jackalope

--- src/Phpcr/PullRequestManager.php
+++ src/Phpcr/PullRequestManager.php
@@ -3,9 +3,12 @@
 namespace PrBuilder\Phpcr;
 
 use PHPCR\SessionInterface;
+use PHPCR\NodeInterface;
 
 class PullRequestManager
 {
+    const PR_NODENAME_FORMAT = 'pr-%s';
+
     private $session;
 
     public function __construct(SessionInterface $session)
@@ -17,12 +20,48 @@ class PullRequestManager
     {
         $root = $this->session->getRootNode();
         if (!$root->hasNode($name)) {
-            $root->addNode($name);
+            $node = $root->addNode($name);
+        } else {
+            $node = $root->getNode($name);
         }
+
+        return $node;
     }
 
-    public function registerPullRequest(array $json)
+    public function registerPayload(array $payload)
     {
-        var_dump($json);die();;
+        $rootNode = $this->getRootNode('pr');
+        $number = $payload['number'];
+        $nodeName = sprintf(self::PR_NODENAME_FORMAT, $number);
+
+        if (!$rootNode->hasNode($nodeName)) {
+            $pullRequestNode = $rootNode->addNode($nodeName);
+        } else {
+            $pullRequestNode = $rootNode->getNode($nodeName);
+        }
+
+        $pullRequest = $payload['pull_request'];
+
+        $this->serializePayload($pullRequestNode, $pullRequest);
+
+        $this->session->save();
+    }
+
+    private function serializePayload(NodeInterface $node, array $pullRequest)
+    {
+        foreach ($pullRequest as $key => $value) {
+            if (is_array($value)) {
+                if ($node->hasNode($key)) {
+                    $child = $node->getNode($key);
+                } else {
+                    $child = $node->addNode($key);
+                }
+
+                $this->serializePayload($child, $value);
+                continue;
+            }
+
+            $node->setProperty($key, $value);
+        }
     }
 }
--- src/Web/Application.php
+++ src/Web/Application.php
@@ -22,9 +22,15 @@ class Application extends SilexApplication
     {
         $app = $this;
         $app['debug'] = true;
-        $this->post('/pr', function (Request $request) use ($app) {
-            print_r($_POST);
-            $app['manager.pull_request'];
+        $this->post('/pr', function () use ($app) {
+            $payload = json_decode(file_get_contents('php://input'), true);
+
+            if (!$payload) {
+                throw new \Exception('Could not json_decode the payload');
+            }
+
+            $prManager = $app['manager.pull_request'];
+            $prManager->registerPayload($payload);
             return new Response('ok');
         });
     }
